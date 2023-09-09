0f8cbaa290f9181ca706d5aac4ad74f53867e556
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-09 00:43:53 -0300

    first commit

---
+++ CRUD_ASP_NET_MS-SQL/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_ASP_NET_MS_SQL.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
