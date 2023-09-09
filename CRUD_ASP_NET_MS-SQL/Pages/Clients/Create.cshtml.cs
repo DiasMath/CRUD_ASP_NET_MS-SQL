1a11be4c6fde98d78407497b467a343e7e3afb24
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-09 02:01:55 -0300

    adicionando formulario POST

---
+++ CRUD_ASP_NET_MS-SQL/Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_ASP_NET_MS_SQL.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
