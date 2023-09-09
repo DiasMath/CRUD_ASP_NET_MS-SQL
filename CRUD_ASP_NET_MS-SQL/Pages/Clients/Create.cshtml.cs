e47e6c3dfd9d2a7f431f5c6960d078ebe9bc6a50
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-09 02:35:32 -0300

    adicionando try/catch para inserir cliente no banco

--- CRUD_ASP_NET_MS-SQL/Pages/Clients/Create.cshtml.cs
+++ CRUD_ASP_NET_MS-SQL/Pages/Clients/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 using static CRUD_ASP_NET_MS_SQL.Pages.Clients.IndexModel;
 
 namespace CRUD_ASP_NET_MS_SQL.Pages.Clients;
@@ -31,9 +32,42 @@ public class CreateModel : PageModel
             return;
         }
 
+        // salvar o cliente no DB
+        try
+        {
+            String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=MyBarberShop;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String sql = "INSERT INTO clients " +
+                             "(name, email, phone, address, appointment, service) VALUES" +
+                             "(@name, @email, @phone, @address, @appointment, @service);";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@name", clientInfo.name);
+                    command.Parameters.AddWithValue("@email", clientInfo.email);
+                    command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                    command.Parameters.AddWithValue("@address", clientInfo.address);
+                    command.Parameters.AddWithValue("@appointment", clientInfo.appointment);
+                    command.Parameters.AddWithValue("@service", clientInfo.service);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            errorMessage = ex.Message;
+            return;
+        }
+
+
         clientInfo.name = ""; clientInfo.email = ""; clientInfo.phone = "";
         clientInfo.address = ""; clientInfo.appointment = ""; clientInfo.service = "";
 
         successMessage = "Novo cliente adicionado!";
+
+        Response.Redirect("/Clients/Index");
     }
 }
