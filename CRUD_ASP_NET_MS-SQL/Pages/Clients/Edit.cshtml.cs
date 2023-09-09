69d3444e2d8d1b85579e35ce706ba124b551eac6
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-09 03:47:40 -0300

    finalizando edit

--- CRUD_ASP_NET_MS-SQL/Pages/Clients/Edit.cshtml.cs
+++ CRUD_ASP_NET_MS-SQL/Pages/Clients/Edit.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 using static CRUD_ASP_NET_MS_SQL.Pages.Clients.IndexModel;
 
 namespace CRUD_ASP_NET_MS_SQL.Pages.Clients;
@@ -16,14 +18,90 @@ public class EditModel : PageModel
 
         try
         {
+            String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=MyBarberShop;Integrated Security=True";
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String sql = "SELECT * FROM clients WHERE id=@id";
 
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            clientInfo.id = "" + reader.GetInt32(0);
+                            clientInfo.name = reader.GetString(1);
+                            clientInfo.email = reader.GetString(2);
+                            clientInfo.phone = reader.GetString(3);
+                            clientInfo.address = reader.GetString(4);
+                            clientInfo.appointment = reader.GetString(5);
+                            clientInfo.service = reader.GetString(6);
+                        }
+                    }
+                }
+            }
         }
         catch (Exception ex)
         {
             errorMessage = ex.Message;
         }
 
+    }
 
+    public void OnPost()
+    {
+        clientInfo.id = Request.Form["id"];
+        clientInfo.name = Request.Form["name"];
+        clientInfo.email = Request.Form["email"];
+        clientInfo.phone = Request.Form["phone"];
+        clientInfo.address = Request.Form["address"];
+        clientInfo.appointment = Request.Form["appointment"];
+        clientInfo.service = Request.Form["service"];
+
+        if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||
+           clientInfo.phone.Length == 0 || clientInfo.address.Length == 0 ||
+           clientInfo.appointment.Length == 0 || clientInfo.service.Length == 0)
+        {
+            errorMessage = "Preencha todos os campos!";
+            return;
+        }
+
+        try
+        {
+            String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=MyBarberShop;Integrated Security=True";
+        
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                String sql = "UPDATE clients " +
+                             "SET name=@name, email=@email, phone=@phone, address=@address, appointment=@appointment, service=@service " +
+                             "WHERE id=@id";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@name", clientInfo.name);
+                    command.Parameters.AddWithValue("@email", clientInfo.email);
+                    command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                    command.Parameters.AddWithValue("@address", clientInfo.address);
+                    command.Parameters.AddWithValue("@appointment", clientInfo.appointment);
+                    command.Parameters.AddWithValue("@service", clientInfo.service);
+                    command.Parameters.AddWithValue("@id", clientInfo.id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            errorMessage = ex.Message;
+            return;
+        }
+
+        Response.Redirect("/Clients/Index");
     }
 }
